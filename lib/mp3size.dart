fcfcbe4331978981c05c4d846e7ab63a5daa6058
igorskyflyer <igor.dvlpr@gmail.com>
2021-06-21 03:41:57 +0200

    Add mp3size.dart

---
+++ lib/mp3size.dart
@@ -0,0 +1,14 @@
+///
+/// *mp3size*
+///
+/// Calculates an estimated file size of MP3 files.
+///
+/// Author: Igor Dimitrijević <igor.dvlpr@gmail.com>, 2021.
+/// Version: 1.1.2
+/// License: MIT, see LICENSE.txt.
+///
+/// Note: does NOT validate any input, that's up to you. :)
+///
+library mp3size;
+
+export 'src/mp3size_base.dart';
